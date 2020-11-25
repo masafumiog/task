f2bb2db4ce7ff697dde2e4855d4407cf2829720c
masafumiog <gurasum041219@gmail.com>
2020-11-25 17:06:12 +0900

    修正

--- question2.rb
+++ question2.rb
@@ -1,14 +1,29 @@
 # 【問題文】
 # ・言語はRubyになります
 # 【問題２：APIの利用】
-# ・下記のダミーAPIのエンドポイントからデータを取得してください
-# 　http://dummy.restapiexample.com/api/v1/employees
-# 　※通信で使うライブラリに制限はありません
-# ・取得結果から、"employee_salary"が"300,000"以上のデータのみ抽出してください
-# ・上記で抽出した配列を、そのまま出力してください
+
+# 【抽出する条件】
+# completed が false の物を抽出して出力
+# ＝＝＝
+
+# あと、HashとArrayの違いを明確にすることをお勧めします。
+# 回答お待ちしてますね！
 
 require 'net/http' 
 require 'uri'
 require "json" 
 
-res = Net::HTTP.get(URI.parse('http://zipcloud.ibsnet.co.jp/api/search?zipcode=6695221'))
+url = URI.parse("https://jsonplaceholder.typicode.com/todos")
+
+https = Net::HTTP.new(url.host, url.port)
+
+https.use_ssl = true
+
+req = Net::HTTP::Get.new(url.path)
+
+res = https.request(req)
+
+
+hash = JSON.parse(res.body)
+
+puts hash.select{|a| a.include?("false")}
